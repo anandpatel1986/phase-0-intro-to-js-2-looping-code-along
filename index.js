d2e1eabd8d69bdc5c512611f70eeb3133a00ae6b
Anandkumar Patel <avpatel2005@gmail.com>
2023-03-07 06:36:14 -0500

    creates for and whole loops

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solutions in this file
+function writeCards(names,eventName){
+    const newArray = [];
+    for(let i = 0; i < names.length; i++){
+        newArray[i] = `Thank you, ${names[i]}, for the wonderful ${eventName} gift!`;
+    }
+
+    return newArray;
+}
+
+function countDown(count){
+   
+    while(count>=0){
+        console.log(count);
+        count--;
+    }
+
+}
+
+const names = ["Guadalupe", "Ollie", "Aki"];
+const eventName = "surprise";
+
+//const returnedArray = writeCards(names, eventName);
+writeCards(names, eventName);
+countDown(10);
